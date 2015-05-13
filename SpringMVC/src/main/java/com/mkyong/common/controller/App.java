a385a1f5dad66b74fca59c25820d32d795eee551
venkatasalam <venkatasalam.eswaramoorthy@photoninfotech.net>
2015-05-13 19:09:39 +0530

    test case and sonar  added

---
+++ SpringMVC/src/main/java/com/mkyong/common/controller/App.java
@@ -0,0 +1,16 @@
+package com.mkyong.common.controller;
+
+public class App{
+	 
+	public static String getHelloWorld() {
+ 
+		return "Hello World";
+ 
+	}
+ 
+	public static String getHelloWorld2() {
+ 
+		return "Hello World 2";
+ 
+	}
+}
